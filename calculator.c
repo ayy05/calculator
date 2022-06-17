80fced9c80fe14fc354ac54e5cfaca4a078bb075
Annabel <annabel.getemail@gmail.com>
2022-06-17 16:37:19 -0700

    separates numbers into an array of strings

--- calculator.c
+++ calculator.c
@@ -1,39 +1,45 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
-
 
 int main(int argc, char *argv[]) {
     if (argc == 2) {
         char *line = argv[1];
-        char *c = *d = argv[1];
         int length = strlen(line);
+        char buffer[100];
         char *input[100];
-        int i = 0;
+        char *curr = buffer;
+        char c;
+        int strnum = 0;
+        int i;
+
         printf("input: %s\n", line);
-        printf("input length: %d\n", (int) strlen(line));
+        printf("input length: %d\n", length);
 
-        while(*c != '\0') {
-            input[i++] = c;
+        while ((c = *line++) != '\0') {
+            input[strnum++] = curr;
 
-            if(isdigit(*c)) {
-                printf("Digit: %c\n", *c++);
-                while(isdigit(*c)) {
-                    printf("Digit: %c\n", *c++);
+            if (isdigit(c)) {
+                *curr++ = c;
+                printf("Digit: %c\n", c);
+                while (isdigit(*line)) {
+                    c = *line++;
+                    printf("Digit: %c\n", c);
+                    *curr++ = c;
                 }
-                
-            }
-
-            if(!isdigit(*c)) {
-                printf("not digit: %c\n", *c++);
+                *curr++ = '\0';
+            } else {
+                printf("not digit: %c\n", c);
+                *curr++ = c;
+                *curr++ = '\0';
             }
         }
 
-
-
-
-
+        printf("\nfound:\n");
+        for (i = 0; i < strnum; i++) {
+            printf("%d: %s\n", i, input[i]);
+        }
     }
     return 0;
 }
